3907aff6a53ebb8d80a990969cd6eae68948e445
yizhiwang96 <wyz_pku@163.com>
2021-09-28 20:23:34 +0800

    test animation

--- imgs/animation.py
+++ imgs/animation.py
@@ -134,7 +134,7 @@ for fontid in fontid_list:
             fout.write('.pen {' + '\n')
             fout.write('\t' + 'stroke-dashoffset: 0;' + '\n')
             fout.write('\t' + 'animation-duration: 10s;' + '\n')
-            fout.write('\t' + 'animation-iteration-count: 1;' + '\n')
+            fout.write('\t' + 'animation-iteration-count: 1000;' + '\n')
             fout.write('\t' + 'animation-timing-function: ease;' + '\n')
             fout.write('}' + '\n')
 
